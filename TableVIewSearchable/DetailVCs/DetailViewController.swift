a655701d419e83b580d44d0966530e7867504b0a
tesddev <tesleem.amuda@gmail.com>
2022-06-10 21:27:16 +0100

    add new `DetailViewController`

---
+++ TableVIewSearchable/DetailVCs/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  TableVIewSearchable
+//
+//  Created by GIGL iOS on 09/06/2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
